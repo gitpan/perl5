da3fea87a74202ed2332256a01c35db7a0cb8de5
Ingy döt Net <ingy@cpan.org>
2011-06-05 16:08:50 +0000

    Import of INGY/perl5-0.02 from CPAN.
    gitpan-cpan-distribution: perl5
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         INGY/perl5-0.02.tar.gz
    gitpan-cpan-author:       INGY
    gitpan-cpan-maturity:     released

--- inc/Module/Package.pm
+++ inc/Module/Package.pm
@@ -22,10 +22,12 @@ BEGIN {
 
 sub import {
     my $class = shift;
-    eval "use inc::Module::Install 1.01 (); 1" or $class->error($@);
+    # Placate a check in Module::Install
+    $INC{'inc/Module/Install.pm'} = __FILE__;
+    eval "use Module::Install 1.01 (); 1" or $class->error($@);
 
     package main;
-    inc::Module::Install->import();
+    Module::Install->import();
     eval {
         module_package_internals_version_check($Module::Package::VERSION);
         module_package_internals_init(@_);
@@ -46,7 +48,13 @@ sub error {
         die <<"...";
 This should not have happened. Hopefully this dump will explain the problem:
 
+inc::Module::Package: $inc::Module::Package::VERSION
+Module::Package: $Module::Package::VERSION
+inc::Module::Install: $inc::Module::Install::VERSION
+Module::Install: $Module::Install::VERSION
+
 Error: $error
+
 %INC:
 $dump1
 \@INC:
